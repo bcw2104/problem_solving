6a1937a57b9076ae3f08370c2ffd31ac6480911a
chealwoobang <bcw2104@naver.com>
2020-07-13 00:40:42 +0900

    빠른 A+B

---
+++ Beakjoon/15552.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int t;
+	int a, b;
+	cin >> t;
+
+	for (int i = 1; i <= t; i++) {
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+
+	return 0;
+}

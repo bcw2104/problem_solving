4ce81bf7fbbd3a8aafd11c46ffacb25180c682c4
chealwoobang <bcw2104@naver.com>
2020-07-22 14:42:54 +0900

    N과 M (2)

---
+++ Beakjoon/15650.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+using namespace std;
+
+void func(int* numbers, int* list, int n, int m, int index) {
+	if (index == m) {
+		for (int i = 0; i < m; i++) {
+			cout << list[i] << " ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			if (index == 0 || (numbers[i] != 0 && numbers[i] > list[index - 1])) {
+				if (n - i >= m - index) {
+					list[index] = numbers[i];
+					numbers[i] = 0;
+					func(numbers, list, n, m, index + 1);
+					numbers[i] = i + 1;
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	int* list;
+	int* numbers;
+	cin >> n >> m;
+
+	list = new int[m];
+	numbers = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		numbers[i] = i + 1;
+	}
+
+	func(numbers, list, n, m, 0);
+
+	return 0;
+}

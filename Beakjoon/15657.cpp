553fd8a8f9ef19fc8c1da4ada452c598e0ad2365
bcw2104 <41840699+bcw2104@users.noreply.github.com>
2021-07-09 16:10:22 +0900

    N과 M (8)

---
+++ Beakjoon/15657.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int n, m;
+int arr[8];
+int temp[8];
+
+void func(int cnt, int pre)
+{
+    if (cnt < m)
+    {
+        for (int i = pre; i < n; i++)
+        {
+            temp[cnt] = arr[i];
+            func(cnt + 1, i);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << temp[i] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    func(0, 0);
+    return 0;
+}
